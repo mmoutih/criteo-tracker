c1d73f7a41527363b1dc04bb128f8fc33f561da2
moutih marouane <m.moutih@gmail.com>
2023-07-12 19:13:32 +0100

    add to card event case with checkin and checkout

--- tests/AddToCartEventTest.php
+++ tests/AddToCartEventTest.php
@@ -6,6 +6,7 @@ use Mmoutih\CriteoTracker\TagsEvents\AccountEvent;
 use Mmoutih\CriteoTracker\TagsEvents\SiteTypeEvent;
 use Mmoutih\CriteoTracker\TagsEvents\AddToCartEvent;
 use Mmoutih\CriteoTracker\TagsEvents\ViewSearchEvent;
+use Mmoutih\CriteoTracker\Exceptions\InvalidArgumentException;
 
 class AddToCartEventTest extends CriteoTagTestCase
 {
@@ -23,18 +24,19 @@ class AddToCartEventTest extends CriteoTagTestCase
     
     public function testAddToCartEventWithInvalidItem()
     {
-        // $this->expectException(InvalidArgumentException::class);
-        // $this->expectExceptionMessage("Invalid id, id should be alphanumerical value.");
-        // $this->expectExceptionCode(5);
-        // $this->loader->addToCartEvent([],)->getEvents();
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Item id is not a valid id");
+        $this->expectExceptionCode(5);
+        $this->loader->addToCartEvent([])->getEvents();
     }
 
     public function testAddToCartEventWithDate()
     {
-        // $events = $this->loader->viewItemPage(itemId: 1,checkin:"06-07-2023",checkout:"12-07-2023")->getEvents();
-        // $this->assertTrue($this->hasEvent($events,AccountEvent::class));
-        // $this->assertTrue($this->hasEvent($events,SiteTypeEvent::class));
-        // $this->assertTrue($this->hasEvent($events,AddToCartEvent::class));
-        // $this->assertTrue($this->hasEvent($events,ViewSearchEvent::class));
+        $item =  ["id" => "prudct_id_1", "price" => 200, "quantity"=> 1];
+        $events = $this->loader->addToCartEvent($item, checkin:"06-07-2023", checkout:"12-07-2023")->getEvents();
+        $this->assertTrue($this->hasEvent($events,AccountEvent::class));
+        $this->assertTrue($this->hasEvent($events,SiteTypeEvent::class));
+        $this->assertTrue($this->hasEvent($events,AddToCartEvent::class));
+        $this->assertTrue($this->hasEvent($events,ViewSearchEvent::class));
     }
 }
